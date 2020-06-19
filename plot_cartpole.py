d7629142eb0febb4f592e98e1e72fd4c095c431c
nuclearavocado <ostark@protonmail.ch>
2020-06-19 13:29:21 +0100

    Improved formulae for system mechanics

--- plot_cartpole.py
+++ plot_cartpole.py
@@ -8,7 +8,7 @@ import matplotlib.lines as mlines # For drawing lines
 import matplotlib.patches as mpatches # For drawing rectangles, circles etc.
 from matplotlib import animation # For animating
 
-def draw_cartpole(ax, goal, x, theta, M, L):
+def draw_cartpole(ax, goal, x, theta, m_c, l):
     """
         Animation function to plot the CartPole system that is passed to
         `matplotlib.animation.FuncAnimation`
@@ -17,8 +17,8 @@ def draw_cartpole(ax, goal, x, theta, M, L):
             goal (int): target location for the CartPole to reach.
             x (int): x-axis position of the cart.
             theta (int): angle of the pole.
-            M (int): mass of the cart.
-            L (int): length of the pole.
+            m_c (int): mass of the cart.
+            l (int): length of the pole.
         # Returns:
             artists (list): list of shapes etc. to plot
     """
@@ -27,28 +27,28 @@ def draw_cartpole(ax, goal, x, theta, M, L):
 
     # Locations of the cart and the pole
     cart_x, cart_y = x, 0
-    pole_x, pole_y = cart_x + L*sin(theta), cart_y - L*cos(theta)
+    pole_x, pole_y = cart_x + l*sin(theta), cart_y - l*cos(theta)
 
     # Dimensions
-    W = 0.4*sqrt(M) # cart width
-    H = 0.25*sqrt(M) # cart height
+    w = 0.4*sqrt(m_c) # cart width
+    h = 0.25*sqrt(m_c) # cart height
     # Positions
-    pole_x = cart_x + L*sin(theta)
-    pole_y = cart_y - L*cos(theta)
+    pole_x = cart_x + l*sin(theta)
+    pole_y = cart_y - l*cos(theta)
 
     # Shapes
     # Draw goal
     artists.append(ax.add_patch(mpatches.Circle([goal, 0], .08, facecolor=[1, 0, 0], zorder=0)))
     # Draw cart
-    artists.append(ax.add_patch(mpatches.Rectangle([cart_x - W/2, cart_y - H/2], W, H, facecolor=[0, 0, 0], edgecolor='k', linewidth=1.5)))
+    artists.append(ax.add_patch(mpatches.Rectangle([cart_x - w/2, cart_y - h/2], w, h, facecolor=[0, 0, 0], edgecolor='k', linewidth=1.5)))
     # Draw pole
-    artists.append(ax.add_line(mlines.Line2D([cart_x, pole_x], [cart_y + (2*(H/2))/3, pole_y], color=[.8,.6,.4], lw=6, zorder=1)))
+    artists.append(ax.add_line(mlines.Line2D([cart_x, pole_x], [cart_y + (2*(h/2))/3, pole_y], color=[.8,.6,.4], lw=6, zorder=1)))
     # Draw pole joint/axis
-    artists.append(ax.add_patch(mpatches.Circle([cart_x, cart_y + (2*(H/2))/3], .08, facecolor=[.5, .5, .8], zorder=2)))
+    artists.append(ax.add_patch(mpatches.Circle([cart_x, cart_y + (2*(h/2))/3], .08, facecolor=[.5, .5, .8], zorder=2)))
 
     return artists
 
-def animate(goal, x, theta, T, M, L):
+def animate(goal, x, theta, T, m_c, l):
     # Create new figure with white background
     fig = plt.figure(figsize=(8, 5), facecolor='w')
     # Add a subplot with no frame
@@ -65,7 +65,7 @@ def animate(goal, x, theta, T, M, L):
     # Append shapes to a list for ArtistAnimation function
     artists_list = []
     for t in range(0, T):
-        artists_list.append(draw_cartpole(ax, goal, x[t], theta[t], M, L))
+        artists_list.append(draw_cartpole(ax, goal, x[t], theta[t], m_c, l))
     # Exits the animation if window closed
     try:
         anim = animation.ArtistAnimation(fig, artists_list, interval=(1/FPS)*1e3,
